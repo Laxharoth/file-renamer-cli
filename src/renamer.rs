663201a35505e1a34bfba0f3e760ff3f42198ef7
Luis Daniel Espino Mandujano <zeydenlaxharoth@gmail.com>
2025-04-04 10:28:38 -0600

    Pass renamer tests
    - Added automata to catch positional wildcard replacements
    - Finish new() function
    - finish get_fixed_strings() function

--- src/renamer.rs
+++ src/renamer.rs
@@ -6,10 +6,111 @@ struct Renamer{
     pub PositionsOrder: Vec<usize>,
 }
 
+fn get_char(string:&String, index:&usize)->char{string.chars().nth(*index).unwrap()}
+
+enum AutomataResult{
+    Success(usize),
+    Failure,
+}
+
+fn selector_wrapper_automata(
+    end_index: &mut usize,
+    string_representation: &String,
+    position_select_wrapper: &(char,char)
+) -> AutomataResult{
+    // ( -> space -> number -> space -> )
+    let mut state = 1;
+    let mut position = 0;
+    loop {
+        *end_index += 1;
+        match &state {
+            1 =>{
+                let character = get_char(string_representation, end_index);
+                if character == ' '{
+                    continue;
+                }
+                if character.is_numeric(){
+                    state = 2;
+                    position = character.to_digit(10).unwrap() as usize;
+                    continue;
+                }
+                return AutomataResult::Failure;
+            },
+            2 =>{
+                let character = get_char(string_representation, &end_index);
+                if character.is_numeric(){
+                    position = position * 10 + character.to_digit(10).unwrap() as usize;
+                    continue;
+                }
+                if character == ' '{
+                    state = 3;
+                    continue;
+                }
+                if character == position_select_wrapper.1{
+                    return AutomataResult::Success(position);
+                }
+                return AutomataResult::Failure;
+            },
+            3 =>{
+                let character = get_char(string_representation, &end_index);
+                if character == ' '{
+                    continue;
+                }
+                if character == position_select_wrapper.1{
+                    return AutomataResult::Success(position);
+                }
+                return AutomataResult::Failure;
+            },
+            _ => {
+                return AutomataResult::Failure;
+            }
+        }
+    }
+
+    return AutomataResult::Success(0);
+}
+
 impl Renamer {
     pub fn new(StringRepresentation:String, WildcardChar:char, PositionSelectWrapper:(char,char)) -> Self{
         let mut FixedStrings: Vec<String> = Vec::new();
         let mut PositionsOrder: Vec<usize> = Vec::new();
+
+        let mut start_index = 0;
+        let mut end_index = 0;
+        let mut wildcard_counter = 0;
+
+        while end_index < StringRepresentation.len() {
+            // Case where wildcard is found
+            if get_char(&StringRepresentation, &end_index) == WildcardChar{
+                FixedStrings.push(StringRepresentation[start_index..end_index].to_string());
+                start_index = end_index + 1;
+                end_index = end_index + 1;
+                PositionsOrder.push(wildcard_counter);
+                wildcard_counter += 1;
+                continue;
+            }
+            // Case where position select wrapper could found
+            if get_char(&StringRepresentation, &end_index) == PositionSelectWrapper.0{
+                // Call automata to find the end of the position select wrapper
+                let starting_position = end_index;
+                match selector_wrapper_automata(
+                    &mut end_index,
+                    &StringRepresentation, 
+                    &PositionSelectWrapper
+                ){
+                    AutomataResult::Success(position)=>{
+                        FixedStrings.push(StringRepresentation[start_index..starting_position].to_string());
+                        PositionsOrder.push(position);
+                        start_index = end_index + 1;
+                        end_index = end_index + 1;
+                        continue;
+                    },
+                    AutomataResult::Failure =>{}
+                }
+            }
+            end_index += 1;
+        }
+        FixedStrings.push(StringRepresentation[start_index..end_index].to_string());
 
         Renamer { 
             StringRepresentation, 
@@ -20,10 +121,20 @@ impl Renamer {
         }
     }
 
+    pub fn get_fixed_strings(&self) -> Vec<String> {
+        self.FixedStrings.clone()
+    }
 
     pub fn generate_rename_filename(&self, filename:&String, wildcard_catched: &Vec<String>) -> String{
+        let mut result = String::new();
 
-        return "".to_string();
+        for i in 0..self.PositionsOrder.len(){
+            result.push_str(self.FixedStrings[i].as_str());
+            result.push_str(wildcard_catched[self.PositionsOrder[i]].as_str());
+        }
+        result.push_str(self.FixedStrings[self.FixedStrings.len()-1].as_str());
+
+        result
     }
 }
 
